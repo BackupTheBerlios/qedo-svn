1a9295a551dbd89b047df12e09938f221f98f57b
tom <tom@798282e8-cfd4-0310-a90d-ae7fb11434eb>
2002-10-13 12:37:01 +0000

    no message

--- trunk/examples/general/philosopher/ExecutorModule/ExecutorValuetypes.h
+++ trunk/examples/general/philosopher/ExecutorModule/ExecutorValuetypes.h
@@ -1,10 +1,24 @@
-//
-// TETRA
-// Example 1 for Stream Container Implementation
-// (C)2000-2002 Humboldt University Berlin, Department of Computer Science
-//
-// $Id: ExecutorValuetypes.h,v 1.1 2002/10/13 12:08:44 tom Exp $
-//
+/***************************************************************************/
+/* Qedo - Qualitiy of Service Enabled Distributed Objects                  */
+/*                                                                         */
+/* http://qedo.berlios.de/                                                 */
+/*                                                                         */
+/* Copyright (C) 2002 by the Qedo Team                                     */
+/*                                                                         */
+/* This library is free software; you can redistribute it and/or           */
+/* modify it under the terms of the GNU Lesser General Public              */
+/* License as published by the Free Software Foundation; either            */
+/* version 2.1 of the License, or (at your option) any later version.      */
+/*                                                                         */
+/* This library is distributed in the hope that it will be useful,         */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of          */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU        */
+/* Lesser General Public License for more details.                         */
+/*                                                                         */
+/* You should have received a copy of the GNU Lesser General Public        */
+/* License along with this library; if not, write to the Free Software     */
+/* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA */
+/***************************************************************************/
 
 #ifndef __EXECUTOR_VALUETYPES_H__
 #define __EXECUTOR_VALUETYPES_H__
