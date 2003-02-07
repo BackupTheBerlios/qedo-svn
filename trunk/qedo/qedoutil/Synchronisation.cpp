bd50ba29d36cebc9cd4f66a816dd8047f79548ad
tom <tom@798282e8-cfd4-0310-a90d-ae7fb11434eb>
2003-02-07 13:21:18 +0000

    TAO configuration added

---
+++ trunk/qedo/qedoutil/Synchronisation.cpp
@@ -0,0 +1,122 @@
+/***************************************************************************/
+/* Qedo - Quality of Service Enabled Distributed Objects                   */
+/*                                                                         */
+/* http://qedo.berlios.de/                                                 */
+/*                                                                         */
+/* Copyright (C) 2002 by the Qedo Team                                     */
+/*                                                                         */
+/* This library is free software; you can redistribute it and/or           */
+/* modify it under the terms of the GNU Lesser General Public              */
+/* License as published by the Free Software Foundation; either            */
+/* version 2.1 of the License, or (at your option) any later version.      */
+/*                                                                         */
+/* This library is distributed in the hope that it will be useful,         */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of          */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU        */
+/* Lesser General Public License for more details.                         */
+/*                                                                         */
+/* You should have received a copy of the GNU Lesser General Public        */
+/* License along with this library; if not, write to the Free Software     */
+/* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA */
+/***************************************************************************/
+
+#include "Synchronisation.h"
+
+namespace Qedo {
+qedo_mutex::qedo_mutex() {
+
+#ifdef WIN32
+	m_mutex = CreateMutex(NULL,FALSE,NULL);
+#else
+	pthread_mutex_init(&m_mutex, NULL);
+#endif
+}
+
+qedo_mutex::~qedo_mutex() {
+
+#ifdef WIN32
+#else
+	pthread_mutex_destroy(&m_mutex);
+#endif
+}
+
+void
+qedo_mutex::qedo_lock_object() {
+
+#ifdef WIN32
+	WaitForSingleObject(m_mutex, INFINITE);
+#else
+	pthread_mutex_lock(&m_mutex);
+#endif
+}
+
+void
+qedo_mutex::qedo_unlock_object() {
+
+#ifdef WIN32
+	ReleaseMutex(m_mutex);
+#else
+	pthread_mutex_unlock(&m_mutex);
+#endif
+}
+
+qedo_lock::qedo_lock(const qedo_mutex* m) {
+	m_mutex = const_cast<qedo_mutex* const>(m);
+	m_mutex->qedo_lock_object();
+}
+
+qedo_lock::qedo_lock(qedo_mutex* m) {
+	m_mutex = m;
+	m_mutex->qedo_lock_object();
+}
+
+qedo_lock::~qedo_lock() {
+	m_mutex->qedo_unlock_object();
+}
+
+
+
+
+#ifdef WIN32
+DWORD WINAPI startFunc(LPVOID p) {
+#else
+void* startFunc(void* p) {
+#endif
+
+	t_start* t = (t_start*) p;
+	void* (*f)(void*) = t->p;
+	void* arg = t->a;
+
+	delete t;
+
+	(f)(arg);
+
+	return 0;
+}
+
+void
+qedo_startDetachedThread(void* (*p)(void*), void* arg) {
+
+	t_start* startParams = new t_start;
+	startParams->p = p;
+	startParams->a = arg;
+
+#ifdef WIN32
+	HANDLE th_handle;
+	DWORD th_id;
+	th_handle = CreateThread(NULL,
+							0,
+							startFunc,
+							(LPVOID) startParams,
+							NULL,
+							&th_id);
+#else
+	pthread_t t;
+	pthread_attr_t detached_attr;
+	pthread_attr_init(&detached_attr);
+	pthread_attr_setdetachstate(&detached_attr, PTHREAD_CREATE_DETACHED);
+	pthread_create(&t, &detached_attr, startFunc, startParams);
+#endif
+}
+
+} // end namespace Qedo
---
+++ trunk/qedo/qedoutil/Synchronisation.h
@@ -0,0 +1,91 @@
+/***************************************************************************/
+/* Qedo - Quality of Service Enabled Distributed Objects                   */
+/*                                                                         */
+/* http://qedo.berlios.de/                                                 */
+/*                                                                         */
+/* Copyright (C) 2002 by the Qedo Team                                     */
+/*                                                                         */
+/* This library is free software; you can redistribute it and/or           */
+/* modify it under the terms of the GNU Lesser General Public              */
+/* License as published by the Free Software Foundation; either            */
+/* version 2.1 of the License, or (at your option) any later version.      */
+/*                                                                         */
+/* This library is distributed in the hope that it will be useful,         */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of          */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU        */
+/* Lesser General Public License for more details.                         */
+/*                                                                         */
+/* You should have received a copy of the GNU Lesser General Public        */
+/* License along with this library; if not, write to the Free Software     */
+/* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA */
+/***************************************************************************/
+
+#ifndef __THREADS_H__
+#define __THREADS_H__
+
+
+#ifdef WIN32
+#ifndef TAO_ORB
+#include <windows.h>
+#endif
+#else
+#include <pthread.h>
+#include <signal.h>
+#endif
+
+#include "Util.h"
+
+namespace Qedo {
+class qedo_lock;
+
+class CONTAINERDLL_API qedo_mutex {
+
+	friend class qedo_lock;
+        friend class qedo_cond;
+
+private:
+
+#ifdef WIN32
+	HANDLE m_mutex;
+#else
+	pthread_mutex_t m_mutex;
+#endif
+
+public:
+	qedo_mutex();
+	~qedo_mutex();
+
+	void qedo_lock_object();
+
+	void qedo_unlock_object();
+};
+
+class CONTAINERDLL_API qedo_lock {
+private:
+	qedo_mutex* m_mutex;
+
+public:
+	qedo_lock(const qedo_mutex* m);
+	qedo_lock(qedo_mutex* m);
+	~qedo_lock();
+};
+
+
+
+#ifdef WIN32
+DWORD WINAPI startFunc(LPVOID p);
+#else
+extern "C" void* startFunc(void* p);
+#endif
+
+void
+qedo_startDetachedThread(void* (*p)(void*), void* arg);
+
+struct t_start {
+	void* (*p)(void*);
+	void* a;
+};
+
+} // name space Qedo
+
+#endif
