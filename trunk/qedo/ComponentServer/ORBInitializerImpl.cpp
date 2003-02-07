a7857cc0859c4a9ad213efa081239a06f53f74d8
neubauer <neubauer@798282e8-cfd4-0310-a90d-ae7fb11434eb>
2003-02-07 11:29:40 +0000

    ORBinitializer implementation for home finder registration;

---
+++ trunk/qedo/ComponentServer/ORBInitializerImpl.cpp
@@ -0,0 +1,127 @@
+/***************************************************************************/
+/* Qedo - Quality of Service Enabled Distributed Objects                   */
+/*                                                                         */
+/* http://qedo.berlios.de/                                                 */
+/*                                                                         */
+/* Copyright (C) 2002 by the Qedo Team                                     */
+/*                                                                         */
+/* This program is free software; you can redistribute it and/or modify it */
+/* under the terms of the GNU General Public License as published by the   */
+/* Free Software Foundation; either version 2 of the License,              */
+/* or (at your option) any later version.                                  */
+/*                                                                         */
+/* This program is distributed in the hope that it will be useful,         */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of          */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                    */
+/* See the GNU General Public License for more details.                    */
+/*                                                                         */
+/* You should have received a copy of the GNU General Public License       */
+/* along with this program; if not, write to the Free Software Foundation, */
+/* Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA             */
+/***************************************************************************/
+
+static char rcsid[] = "$Id: ORBInitializerImpl.cpp,v 1.1 2003/02/07 11:29:40 neubauer Exp $";
+
+#include "ORBInitializerImpl.h"
+#include "Output.h"
+
+
+namespace Qedo {
+
+
+ORBInitializerImpl::ORBInitializerImpl()
+{
+    //
+	// Register our ORB initializer
+	//
+    PortableInterceptor::register_orb_initializer (this);
+}
+
+
+ORBInitializerImpl::~ORBInitializerImpl()
+{
+}
+
+
+void 
+ORBInitializerImpl::pre_init (PortableInterceptor::ORBInitInfo_ptr info)
+{
+}
+
+
+void 
+ORBInitializerImpl::post_init (PortableInterceptor::ORBInitInfo_ptr info)
+{
+    //
+	// First resolve the Name Service
+	//
+    CORBA::Object_var obj;
+
+	try
+    {
+        obj = info->resolve_initial_references("NameService");
+    }
+    catch (const CORBA::ORB::InvalidName&)
+    {
+		std::cerr << "ORBInitializerImpl: Can't resolve NameService" << std::endl;
+		return;
+    }
+
+    if (CORBA::is_nil(obj.in()))
+    {
+		std::cerr << "ORBInitializerImpl: NameService is a nil object reference" << std::endl;
+		return;
+    }
+
+	try
+	{
+		nameService_ = CosNaming::NamingContext::_narrow(obj.in());
+	}
+	catch (const CORBA::Exception&)
+	{
+		std::cerr << "ORBInitializerImpl: NameService is not running" << std::endl;
+		return;
+	}
+
+    if (CORBA::is_nil(nameService_.in()))
+    {
+		std::cerr << "ORBInitializerImpl: NameService is not a NamingContext object reference" << std::endl;
+		return;
+    }
+
+    //
+	// Resolve the HomeFinder
+	//
+	Qedo_Components::HomeFinder_var home_finder;
+	obj = resolveName("Qedo/HomeFinder");
+
+	if (CORBA::is_nil(obj.in()))
+    {
+		std::cerr << "ORBInitializerImpl: HomeFinder not found" << std::endl;
+		return;
+    }
+
+    try
+    {
+        home_finder = Qedo_Components::HomeFinder::_narrow(obj);
+    }
+    catch (CORBA::SystemException&)
+    {
+        std::cerr << "ORBInitializerImpl: HomeFinder is not running" << std::endl;
+		return;
+    }
+
+	if (CORBA::is_nil(home_finder.in()))
+    {
+		std::cerr << "ORBInitializerImpl: HomeFinder is not running" << std::endl;
+		return;
+    }
+
+	//
+	// register HomeFinder
+	//
+	info->register_initial_reference ("ComponentHomeFinder", home_finder);
+}
+
+
+} // namespace Qedo
---
+++ trunk/qedo/ComponentServer/ORBInitializerImpl.h
@@ -0,0 +1,52 @@
+/***************************************************************************/
+/* Qedo - Quality of Service Enabled Distributed Objects                   */
+/*                                                                         */
+/* http://qedo.berlios.de/                                                 */
+/*                                                                         */
+/* Copyright (C) 2002 by the Qedo Team                                     */
+/*                                                                         */
+/* This program is free software; you can redistribute it and/or modify it */
+/* under the terms of the GNU General Public License as published by the   */
+/* Free Software Foundation; either version 2 of the License,              */
+/* or (at your option) any later version.                                  */
+/*                                                                         */
+/* This program is distributed in the hope that it will be useful,         */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of          */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                    */
+/* See the GNU General Public License for more details.                    */
+/*                                                                         */
+/* You should have received a copy of the GNU General Public License       */
+/* along with this program; if not, write to the Free Software Foundation, */
+/* Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA             */
+/***************************************************************************/
+
+#ifndef __ORB_INITIALIZER_IMPL_H__
+#define __ORB_INITIALIZER_IMPL_H__
+
+#include <OB/CORBA.h>
+#include <OB/PortableInterceptor.h>
+#include "QedoComponents_skel.h"
+#include "NameServiceBase.h"
+#include "Util.h"
+
+
+namespace Qedo {
+
+
+class ORBInitializerImpl : public virtual PortableInterceptor::ORBInitializer,
+						   public virtual NameServiceBase,
+						   public virtual CORBA::LocalObject
+{
+public:
+	ORBInitializerImpl();
+	~ORBInitializerImpl();
+
+	void pre_init (PortableInterceptor::ORBInitInfo_ptr info);
+    void post_init (PortableInterceptor::ORBInitInfo_ptr info);
+};
+
+
+} // namespace
+
+
+#endif
