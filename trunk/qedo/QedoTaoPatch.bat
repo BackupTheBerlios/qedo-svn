7817d3acaccd4081886e4cb7f02fc1c1d1712bbd
ogr <ogr@798282e8-cfd4-0310-a90d-ae7fb11434eb>
2003-04-25 07:45:09 +0000

    copy needed headers from TAO

---
+++ trunk/qedo/QedoTaoPatch.bat
@@ -0,0 +1,8 @@
+copy %ACE_ROOT%\TAO\tao\IFR_Client\IFR_BasicC.h  ComponentIDL\IFR_Basic.h
+copy %ACE_ROOT%\TAO\tao\IFR_Client\IFR_BasicC.h  ComponentIDL\IFR_Basic_skel.h
+
+copy %ACE_ROOT%\TAO\tao\PortableServer\PortableServerC.h  ComponentIDL\PortableServer.h
+copy %ACE_ROOT%\TAO\tao\PortableServer\PortableServerC.h  ComponentIDL\PortableServer_skel.h
+
+copy %ACE_ROOT%\TAO\orbsvcs\orbsvcs\CosNamingC.h  ComponentIDL\CosNaming.h
+copy %ACE_ROOT%\TAO\orbsvcs\orbsvcs\CosNamingC.h  ComponentIDL\CosNaming_skel.h
