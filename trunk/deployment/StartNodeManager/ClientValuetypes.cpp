fce3ddc6aa879955b5f1f08054661ad2d8891b65
tom <tom@798282e8-cfd4-0310-a90d-ae7fb11434eb>
2003-04-27 11:11:14 +0000

    no message

---
+++ trunk/deployment/StartNodeManager/ClientValuetypes.cpp
@@ -0,0 +1,344 @@
+
+static char rcsid[] = "$Id: ClientValuetypes.cpp,v 1.1 2003/04/27 11:03:39 tom Exp $";
+
+
+#include "ClientValuetypes.h"
+
+
+CORBA::LongLong Cookie_impl::cookie_key_ = 0;
+
+//
+// Cookie
+//
+Cookie_impl::Cookie_impl()
+{
+	CORBA::OctetSeq_var octet_key = new CORBA::OctetSeq();
+
+	octet_key->length (8);
+
+	for (unsigned int i = 0; i < 8; i++)
+	{
+		octet_key.inout()[i] = ((char*)(&cookie_key_))[i];
+	}
+
+	cookieValue (octet_key.in());
+
+    ++cookie_key_;
+}
+
+
+Cookie_impl::~Cookie_impl
+()
+{
+}
+
+
+CORBA::Boolean 
+Cookie_impl::equal (Components::Cookie* cook)
+{
+	Cookie_impl* foreign_cook = dynamic_cast<Cookie_impl*>(cook);
+
+	if (! foreign_cook)
+		return false;
+
+	CORBA::OctetSeq x = cookieValue();
+	CORBA::OctetSeq y = foreign_cook->cookieValue();
+
+	for (unsigned int i = 0; i < x.length(); i++)
+	{
+		if (x[i] != y[i])
+			return false;
+    }
+    
+	return true;
+}
+
+
+CORBA::ValueBase*
+CookieFactory_impl::create_for_unmarshal()
+{
+	return new Cookie_impl();
+}
+
+
+//
+// PortDescription
+//
+PortDescription_impl::PortDescription_impl (const char* name, const char* type_id)
+#ifndef VC6
+: PortDescription (name, type_id)
+#endif
+{
+}
+
+
+PortDescription_impl::PortDescription_impl()
+{
+}
+
+
+PortDescription_impl::~PortDescription_impl()
+{
+}
+
+
+CORBA::ValueBase* 
+PortDescriptionFactory_impl::create_for_unmarshal
+()
+{	
+	return new PortDescription_impl();
+}
+
+
+//
+// FacetDescription
+//
+FacetDescription_impl::FacetDescription_impl (const char* name, 
+											  const char* type_id, 
+											  CORBA::Object_ptr ref)
+#ifndef VC6
+: FacetDescription (name, type_id, ref)
+#endif
+{
+}
+
+
+FacetDescription_impl::FacetDescription_impl()
+{
+}
+
+
+FacetDescription_impl::~FacetDescription_impl()
+{
+}
+
+
+CORBA::ValueBase* 
+FacetDescriptionFactory_impl::create_for_unmarshal()
+{	
+	return new FacetDescription_impl();
+}
+
+
+//
+// ConnectionDescription
+//
+ConnectionDescription_impl::ConnectionDescription_impl (Components::Cookie* ck, CORBA::Object_ptr objref)
+#ifndef VC6
+: ConnectionDescription (ck, objref)
+#endif
+{
+}
+
+
+ConnectionDescription_impl::ConnectionDescription_impl()
+{
+}
+
+
+ConnectionDescription_impl::~ConnectionDescription_impl
+()
+{
+}
+
+
+CORBA::ValueBase* 
+ConnectionDescriptionFactory_impl::create_for_unmarshal
+()
+{	
+	return new ConnectionDescription_impl();
+}
+
+
+//
+// ReceptacleDescription
+//
+ReceptacleDescription_impl::ReceptacleDescription_impl (const char* name, 
+														const char* type_id, 
+														CORBA::Boolean is_multiple, 
+														const Components::ConnectedDescriptions& connections)
+#ifndef VC6
+: ReceptacleDescription (name, type_id, is_multiple, connections)
+#endif
+{
+}
+
+
+ReceptacleDescription_impl::ReceptacleDescription_impl()
+{
+}
+
+
+ReceptacleDescription_impl::~ReceptacleDescription_impl()
+{
+}
+
+
+CORBA::ValueBase* 
+ReceptacleDescriptionFactory_impl::create_for_unmarshal()
+{	
+	return new ReceptacleDescription_impl();
+}
+
+
+//
+// ConsumerDescription
+//
+ConsumerDescription_impl::ConsumerDescription_impl (const char* name, 
+													const char* type_id, 
+													Components::EventConsumerBase_ptr consumer)
+#ifndef VC6
+: ConsumerDescription (name, type_id, consumer)
+#endif
+{
+}
+
+
+ConsumerDescription_impl::ConsumerDescription_impl()
+{
+}
+
+
+ConsumerDescription_impl::~ConsumerDescription_impl()
+{
+}
+
+
+CORBA::ValueBase* 
+ConsumerDescriptionFactory_impl::create_for_unmarshal()
+{	
+	return new ConsumerDescription_impl();
+}
+
+
+//
+// EmitterDescription
+//
+EmitterDescription_impl::EmitterDescription_impl (const char* name, 
+												  const char* type_id, 
+												  Components::EventConsumerBase_ptr consumer)
+#ifndef VC6
+: EmitterDescription (name, type_id, consumer)
+#endif
+{
+}
+
+
+EmitterDescription_impl::EmitterDescription_impl()
+{
+}
+
+
+EmitterDescription_impl::EmitterDescription_impl (const EmitterDescription_impl& emitter_desc)
+{
+}
+
+
+EmitterDescription_impl::~EmitterDescription_impl()
+{
+}
+
+
+CORBA::ValueBase* 
+EmitterDescriptionFactory_impl::create_for_unmarshal()
+{	
+	return new EmitterDescription_impl();
+}
+
+
+//
+// SubscriberDescription
+//
+SubscriberDescription_impl::SubscriberDescription_impl (const char* name, 
+						   const char* type_id, 
+						   Components::Cookie* ck, 
+						   Components::EventConsumerBase_ptr consumer)
+#ifndef VC6
+: SubscriberDescription (name, type_id, ck, consumer)
+#endif
+{
+}
+
+
+SubscriberDescription_impl::SubscriberDescription_impl()
+{
+}
+
+
+SubscriberDescription_impl::~SubscriberDescription_impl()
+{
+}
+
+
+CORBA::ValueBase* 
+SubscriberDescriptionFactory_impl::create_for_unmarshal()
+{	
+	return new SubscriberDescription_impl();
+}
+
+
+//
+// ComponentPortDescription
+//
+ComponentPortDescription_impl::ComponentPortDescription_impl (const Components::FacetDescriptions& facets,
+															  const Components::ReceptacleDescriptions& receptacles,
+															  const Components::ConsumerDescriptions& consumers,
+															  const Components::EmitterDescriptions& emitters,
+															  const Components::SubscriberDescriptions& publishers)
+#ifndef VC6
+: ComponentPortDescription (facets, receptacles, consumers, emitters, publishers)
+#endif
+{
+}
+
+
+ComponentPortDescription_impl::ComponentPortDescription_impl()
+{
+}
+
+
+ComponentPortDescription_impl::~ComponentPortDescription_impl()
+{
+}
+
+
+CORBA::ValueBase* 
+ComponentPortDescriptionFactory_impl::create_for_unmarshal()
+{	
+	return new ComponentPortDescription_impl();
+}
+
+
+//
+// ConfigValue
+//
+#if _MSC_VER < 1300
+ConfigValue_impl::ConfigValue_impl (const char* name, CORBA::Any& value)
+{
+	this->name(name);
+	this->value(value);
+}
+#else
+ConfigValue_impl::ConfigValue_impl (const char* name, CORBA::Any& value)
+: ConfigValue (name, value)
+{
+}
+#endif
+
+ConfigValue_impl::ConfigValue_impl()
+{
+}
+
+
+ConfigValue_impl::~ConfigValue_impl
+()
+{
+}
+
+
+CORBA::ValueBase* 
+ConfigValueFactory_impl::create_for_unmarshal
+()
+{	
+	return new ConfigValue_impl();
+}
---
+++ trunk/deployment/StartNodeManager/main.cpp
@@ -0,0 +1,253 @@
+#include "CORBA.h"
+#include "CosNaming.h"
+#include "DCI_Basics_NodeManagerImpl_EQUIVALENT.h"
+#include "ClientValuetypes.h"
+
+
+Components::Deployment::ServerActivator_ptr
+get_server_activator (CORBA::ORB_ptr orb, CosNaming::NamingContext_ptr ns, const char* hostname)
+{
+	std::cout << "Getting Component Server Activator from Qedo/Activators/" << hostname << std::endl;
+
+	CosNaming::Name server_activator_name;
+	server_activator_name.length (3);
+	server_activator_name[0].id = CORBA::string_dup ("Qedo");
+	server_activator_name[0].kind = CORBA::string_dup ("");
+	server_activator_name[1].id = CORBA::string_dup ("Activators");
+	server_activator_name[1].kind = CORBA::string_dup ("");
+	server_activator_name[2].id = CORBA::string_dup (hostname);
+	server_activator_name[2].kind = CORBA::string_dup ("");
+
+	Components::Deployment::ServerActivator_var server_activator;
+	CORBA::Object_var server_activator_obj;
+
+	try
+	{
+		server_activator_obj = ns->resolve (server_activator_name);
+	}
+	catch (CosNaming::NamingContext::NotFound&)
+	{
+		std::cerr << "Component Server Activator not found in Name Service" << std::endl;
+		orb->destroy();
+		exit (1);
+	}
+	catch (CORBA::SystemException&)
+	{
+		std::cerr << "CORBA system exception during resolve()" << std::endl;
+		orb->destroy();
+		exit (1);
+	}
+		
+	try
+	{
+		server_activator = Components::Deployment::ServerActivator::_narrow (server_activator_obj);
+	}
+	catch (CORBA::SystemException&)
+	{
+		std::cerr << "Cannot narrow Component Server Activator" << std::endl;
+		orb->destroy();
+		exit (1);
+	}
+
+	return server_activator._retn();
+}
+
+Components::Deployment::ComponentInstallation*
+get_component_installation(CORBA::ORB_ptr orb, CosNaming::NamingContext_ptr ns, const char* hostname)
+{
+	std::cout << "Getting Component Installation from Qedo/ComponentInstallation/" << hostname << std::endl;
+
+	CosNaming::Name installer_name;
+	installer_name.length (3);
+	installer_name[0].id = CORBA::string_dup ("Qedo");
+	installer_name[0].kind = CORBA::string_dup ("");
+	installer_name[1].id = CORBA::string_dup ("ComponentInstallation");
+	installer_name[1].kind = CORBA::string_dup ("");
+	installer_name[2].id = CORBA::string_dup (hostname);
+	installer_name[2].kind = CORBA::string_dup ("");
+
+	Components::Deployment::ComponentInstallation_var component_installer;
+	CORBA::Object_var component_installer_obj;
+
+	try
+	{
+		component_installer_obj = ns->resolve (installer_name);
+	}
+	catch (CosNaming::NamingContext::NotFound&)
+	{
+		std::cerr << "Component Installer not found in Name Service" << std::endl;
+		orb->destroy();
+		exit (1);
+	}
+	catch (CORBA::SystemException&)
+	{
+		std::cerr << "CORBA system exception during resolve()" << std::endl;
+		orb->destroy();
+		exit (1);
+	}
+		
+	try
+	{
+		component_installer = Components::Deployment::ComponentInstallation::_narrow (component_installer_obj);
+	}
+	catch (CORBA::SystemException&)
+	{
+		std::cerr << "Cannot narrow Component Installer" << std::endl;
+		orb->destroy();
+		exit (1);
+	}
+
+return component_installer._retn();
+}
+
+
+int
+main (int argc, char** argv)
+{
+	std::cout << "Test Client for Stream Container" << std::endl;
+
+	CORBA::ORB_var orb = CORBA::ORB_init (argc, argv);
+
+    orb->register_value_factory ("IDL:omg.org/Components/FacetDescription:1.0", new FacetDescriptionFactory_impl());
+	orb->register_value_factory ("IDL:omg.org/Components/ReceptacleDescription:1.0", new ReceptacleDescriptionFactory_impl());
+    orb->register_value_factory ("IDL:omg.org/Components/SubscriberDescription:1.0", new SubscriberDescriptionFactory_impl());
+    orb->register_value_factory ("IDL:omg.org/Components/EmitterDescription:1.0", new EmitterDescriptionFactory_impl());
+    orb->register_value_factory ("IDL:omg.org/Components/ConsumerDescription:1.0", new ConsumerDescriptionFactory_impl());
+    orb->register_value_factory ("IDL:omg.org/Components/Cookie:1.0", new CookieFactory_impl());
+
+	CosNaming::NamingContext_var ns;
+
+	try
+	{
+		CORBA::Object_var ns_obj = orb->resolve_initial_references ("NameService");
+		ns = CosNaming::NamingContext::_narrow (ns_obj);
+	}
+	catch (CORBA::ORB::InvalidName&)
+	{
+		std::cerr << "Name Service not found" << std::endl;
+		orb->destroy();
+		exit (1);
+	}
+	catch (CORBA::SystemException&)
+	{
+		std::cerr << "Cannot narrow object reference of Name Service" << std::endl;
+		orb->destroy();
+		exit (1);
+	}
+
+	if (CORBA::is_nil (ns))
+	{
+		std::cerr << "Name Service is nil" << std::endl;
+		orb->destroy();
+		exit (1);
+	}
+
+	// Now get the Component Server Activator from the Name Service, use the name TETRA/Activators/<hostname>
+	char hostname[256];
+	if (gethostname (hostname, 256))
+	{
+		std::cerr << "Cannot determine my hostname" << std::endl;
+		orb->destroy();
+		exit (1);
+	}
+	// Get the Server Activator
+	Components::Deployment::ServerActivator_var server_activator = get_server_activator (orb, ns, hostname);
+
+	Components::Deployment::ComponentInstallation_var component_installer = get_component_installation (orb, ns, hostname);
+
+	try {
+#if _WIN32
+		component_installer->install ("NodeManagerImpl/1.0", "DCI_Basics_NodeManagerImpl_SERVANT.dll:create_NodeManagerHomeS:DCI_Basics_NodeManagerImpl_EXECUTOR.dll:create_NodeManagerHomeE");
+#else
+//		component_installer->install ("NodeManagerImpl/1.0", "libCallerS.so:create_CallerHomeS:libCallerE.so:create_CallerHomeE");
+#endif
+	}
+	catch (Components::Deployment::InvalidLocation&)
+	{
+		std::cerr << "Component Installer raised Components::Deployment::InvalidLocation" << std::endl;
+	}
+	catch (Components::Deployment::InstallationFailure&)
+	{
+		std::cerr << "Component Installer raised Components::Deployment::InstallationFailure" << std::endl;
+	}
+
+	// Create Component Server
+	std::cout << "create component server" << std::endl;
+	Components::ConfigValues config;
+	Components::Deployment::ComponentServer_var component_server;
+
+	try
+	{
+		component_server = 	server_activator->create_component_server (config);
+	}
+	catch (CORBA::Exception&)
+	{
+		std::cerr << "Exception during test run" << std::endl;
+		orb->destroy();
+		exit (1);
+	}
+
+	if (CORBA::is_nil (component_server))
+	{
+		std::cerr << "I got a nil reference for the created Component Server" << std::endl;
+		orb->destroy();
+		exit (1);
+	}
+
+	// Create Container
+	std::cout << "create container" << std::endl;
+	Components::Deployment::Container_var container;
+	config.length (1);
+	CORBA::Any any;
+	any <<= "SESSION";
+	config[0] = new ConfigValue_impl ("CONTAINER_TYPE", any);
+
+	try
+	{
+		container = component_server->create_container (config);
+	}
+	catch (CORBA::SystemException&)
+	{
+		std::cerr << "CORBA system exception during creating container" << std::endl;
+		orb->destroy();
+		exit (1);
+	}
+
+	config.length(0);
+
+	Components::CCMHome_var home;
+
+	std::cout << "install home for NodeManager" << std::endl;
+	home = container->install_home ("NodeManagerImpl/1.0", "", config);
+	DCI_Basics::NodeManagerHome_var node_manager_home = DCI_Basics::NodeManagerHome::_narrow (home);
+
+	DCI_Basics::NodeManager_var node_manager;
+	try {
+	node_manager = node_manager_home->create();
+
+	}
+	catch (Components::CreateFailure&)
+	{
+		std::cerr << "Create Failure exception" << std::endl;
+	}
+	catch (CORBA::SystemException& ex)
+	{
+		std::cerr << /*ex <<*/ std::endl;
+	}
+
+	try	{
+		std::cout << "connect NodeManager with ci and csa" << std::endl;
+		// needs to be adapted if other CCM implementations are used.
+		node_manager->connect_orig_component_installation(Components::Deployment::ComponentInstallation::_duplicate(component_installer.in()));
+		node_manager->connect_orig_server_activator(server_activator);
+
+		std::cout << "start NodeManager" << std::endl;
+		node_manager->configuration_complete();
+	}
+	catch (CORBA::SystemException& ex)
+	{
+		std::cerr << /*ex <<*/ std::endl;
+	}
+
+	return 0;
+};
