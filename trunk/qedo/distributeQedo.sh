427756c24d1fe1ac517f1eeb01fb849604704f7c
tom <tom@798282e8-cfd4-0310-a90d-ae7fb11434eb>
2003-05-22 11:11:42 +0000

    *** empty log message ***

--- trunk/qedo/distributeQedo.sh
+++ trunk/qedo/distributeQedo.sh
@@ -1,29 +1,29 @@
 # input Parameter 1 should be the Qedo install directory (e.g. /opt/qedo)
 
 #### start distribution ####
-mkdir -p $2;
+mkdir -p $1;
 #### bin dir ####
-mkdir -p $2/bin;
-cp ComponentInstaller/ci $2/bin;
-cp ComponentServerActivator/csa $2/bin;
-cp ComponentServer/cs $2/bin;
-cp HomeFinder/homefinder $2/bin;
-cp DeploymentClient/deploy $2/bin;
-cp AssemblyFactory/assf $2/bin;
+mkdir -p $1/bin;
+cp ComponentInstaller/ci $1/bin;
+cp ComponentServerActivator/csa $1/bin;
+cp ComponentServer/cs $1/bin;
+cp HomeFinder/homefinder $1/bin;
+cp DeploymentClient/deploy $1/bin;
+cp AssemblyFactory/assf $1/bin;
 
 #### etc dir ####
-mkdir -p $2/etc;
-cp etc/*.* $2/etc;
+mkdir -p $1/etc;
+cp etc/*.* $1/etc;
 
-mkdir -p $2/include;
-cp ComponentIDL/*.h $2/include;
-cp ComponentContainer/*.h $2/include;
+mkdir -p $1/include;
+cp ComponentIDL/*.h $1/include;
+cp ComponentContainer/*.h $1/include;
 
-mkdir -p $2/idl;
-cp ComponentIDL/*.idl $2/idl;
+mkdir -p $1/idl;
+cp ComponentIDL/*.idl $1/idl;
 
-mkdir -p $2/lib;
-cp ComponentIDL/libComponentIDL.so $2/lib;
-cp ComponentContainer/libComponentContainer.so /$2/lib;
+mkdir -p $1/lib;
+cp ComponentIDL/libComponentIDL.so $1/lib;
+cp ComponentContainer/libComponentContainer.so /$1/lib;
 
 
