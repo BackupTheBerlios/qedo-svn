63ff108f8b8dd36a8fe0edfc5d32bee8b4413690
hao <hao@798282e8-cfd4-0310-a90d-ae7fb11434eb>
2003-06-25 14:45:38 +0000

    StorageObjectRef added

--- branches/br_0_1_2_wang/qedo/ComponentIDL/PSSNativeClasses.h
+++ branches/br_0_1_2_wang/qedo/ComponentIDL/PSSNativeClasses.h
@@ -1,6 +1,8 @@
 #ifndef __PSSNATIVECLASSES_H__
 #define __PSSNATIVECLASSES_H__
 
+#include <CORBA.h>
+
 namespace CosPersistentState
 {
 
@@ -45,10 +47,66 @@ class StorageObject : public virtual StorageObjectBase
 		virtual ~StorageObject () {}
 };
 
+class StorageObjectRef
+{
+	public:
+		
+		StorageObjectRef(StorageObject* obj=0) 
+			throw();
+		
+		StorageObjectRef(const StorageObjectRef& ref) // copy constructure
+			throw();
+
+		StorageObjectRef(StorageObject* obj) 
+			throw();
+
+		~StorageObjectRef ();
+
+		StorageObjectRef& operator=(const StorageObjectRef& ref) 
+			throw();
+
+		StorageObjectRef& operator=(StorageObject* obj) 
+			throw();
+
+		void release() 
+			throw();
+
+		StorageObject* deref() 
+			throw(CORBA::SystemException);
+
+		StorageObject* operator->() 
+			throw(CORBA::SystemException); // not const!
+
+		void destroy_object() 
+			throw(CORBA::SystemException);
+
+		Pid* get_pid() const 
+			throw(CORBA::SystemException);
+
+		ShortPid* get_short_pid() const 
+			throw(CORBA::SystemException);
+
+		CORBA::Boolean is_null() const 
+			throw();
+
+		StorageHomeBase_ptr get_storage_home() const
+			throw(CORBA::SystemException);
+
+		static StorageObjectRef _duplicate(StorageObjectRef ref);
+
+		static StorageObjectRef _downcast(StorageObjectRef ref);
+
+
+	public:
+		
+		typedef StorageObject _target_type;
+};
+
 template class<T>
 class Factory
 {
 	public:
+
 		virtual T* create() throw (SystemException) = 0;
 		virtual void _add_ref() {};
 		virtual void _remove_ref() {};
@@ -61,4 +119,5 @@ typedef Factory<Sessio> SessionFactory;
 typedef Factory<SessionPool> SessionPoolFactory;
 
 };
+
 #endif
