b0425c4d91487dc4899b844efb1067081cfc86f9
neubauer <neubauer@798282e8-cfd4-0310-a90d-ae7fb11434eb>
2003-09-26 08:27:15 +0000

    moved deployment functionality to qedoutil;

--- trunk/qedo/DeploymentClient/qdeploy.cpp
+++ trunk/qedo/DeploymentClient/qdeploy.cpp
@@ -21,7 +21,7 @@
 /***************************************************************************/
 
 
-#include "DeploymentClient.h"
+#include "ComponentDeployment.h"
 #include "Util.h"
 #include "version.h"
 #ifdef _WIN32
@@ -30,7 +30,7 @@
 #endif
 
 
-static char rcsid[] UNUSED = "$Id: qdeploy.cpp,v 1.10 2003/09/01 10:47:48 neubauer Exp $";
+static char rcsid[] UNUSED = "$Id: qdeploy.cpp,v 1.11 2003/09/26 08:27:15 neubauer Exp $";
 
 
 /**
@@ -42,8 +42,8 @@ static char rcsid[] UNUSED = "$Id: qdeploy.cpp,v 1.10 2003/09/01 10:47:48 neubau
 void
 printUsage()
 {
-	std::cerr << "usage : deploy [options] <package>" << std::endl;
-	std::cerr << "		  [--help|-h] : print this" << std::endl;
+	std::cerr << "usage : qdeploy [options] <package>" << std::endl;
+	std::cerr << "        [--help|-h] : print this" << std::endl;
 	std::cerr << "        -f : <package> is a local file" << std::endl;
 	std::cerr << "        <package> is the URL of an assembly package" << std::endl;
 }
@@ -55,7 +55,7 @@ printUsage()
 int
 main (int argc, char** argv)
 {
-	std::cout << "Qedo Deployment Client " << QEDO_VERSION << std::endl;
+	std::cout << "qdeploy " << QEDO_VERSION << std::endl;
 
 	//
 	// process arguments
@@ -79,19 +79,14 @@ main (int argc, char** argv)
 		else if(strcmp(option, "-f") == 0)
 		{
 			package = "file:///";
-			char path[1024];
+			package.append( Qedo::getCurrentDirectory() );
 #ifdef _WIN32
-			GetCurrentDirectory(1024, path);
-			package.append(path);
 			std::string::size_type pos = package.find_first_of("\\");
 			while ((pos >= 0) && (pos < package.size()))
 			{
 				package.replace(pos, 1, "/");
 				pos = package.find_first_of("\\");
 			}
-#else
-			getcwd(path,1023);
-			package.append(path);
 #endif
 			package.append("/");
 			package.append(argv[argc - 1]);
@@ -119,22 +114,51 @@ main (int argc, char** argv)
 	factory = new Qedo::CookieFactory_impl();
     orb -> register_value_factory ( "IDL:omg.org/Components/Cookie:1.0", factory );
 
-	Qedo::DeploymentClient* client = new Qedo::DeploymentClient(orb);
-
+	//
+	// deploy the assembly
+	//
+	Qedo::ComponentDeployment ass(package);
 	try
 	{
-		client->initialize();
-		client->create(package);
+		std::cerr << "..... deploy " << package << std::endl;
+		ass.deploy();
 	}
-	catch(Qedo::DeploymentClient::CannotInitialize&)
+	catch(Qedo::ComponentDeployment::DeploymentFailure&)
 	{
-		std::cerr << "Cannot initialize Test Client ... exiting." << std::endl;
+		std::cerr << "Cannot deploy " << package << std::endl;
 		orb->destroy();
 		exit(1);
 	}
 	catch(CORBA::SystemException&)
 	{
-		std::cerr << "CORBA system exception ... exiting." << std::endl;
+		std::cerr << "Cannot deploy " << package << std::endl;
+		orb->destroy();
+		exit(1);
+	}
+
+	//
+	// wait
+	//
+    std::cout << "..... please press any key to destroy the Assembly ..." << std::endl;
+    getchar();
+
+	//
+	// undeploy the assembly
+	//
+	try
+	{
+		std::cerr << "..... undeploy " << package << std::endl;
+		ass.undeploy();
+	}
+	catch(Qedo::ComponentDeployment::DeploymentFailure&)
+	{
+		std::cerr << "Cannot undeploy " << package << std::endl;
+		orb->destroy();
+		exit(1);
+	}
+	catch(CORBA::SystemException&)
+	{
+		std::cerr << "Cannot undeploy " << package << std::endl;
 		orb->destroy();
 		exit(1);
 	}
