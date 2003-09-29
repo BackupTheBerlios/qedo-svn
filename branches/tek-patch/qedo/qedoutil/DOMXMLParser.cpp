6cfc7a0c55faaf6b295633f3a5c525a9f07d7fb8
tom <tom@798282e8-cfd4-0310-a90d-ae7fb11434eb>
2003-09-29 20:01:09 +0000

    *** empty log message ***

--- branches/tek-patch/qedo/qedoutil/DOMXMLParser.cpp
+++ branches/tek-patch/qedo/qedoutil/DOMXMLParser.cpp
@@ -21,7 +21,9 @@
 /***************************************************************************/
 
 #include "DOMXMLParser.h"
-
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
 
 namespace Qedo {
 
