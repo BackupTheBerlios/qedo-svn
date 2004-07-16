f109478d89016d41d6665790fdb158cee194beaa
tom <tom@798282e8-cfd4-0310-a90d-ae7fb11434eb>
2004-07-16 11:56:43 +0000

    first step for new interception points and negotiation

---
+++ trunk/qedo/ComponentContainer/ServantInterceptorDispatcher.cpp
@@ -0,0 +1,191 @@
+/***************************************************************************/
+/* Qedo - Quality of Service Enabled Distributed Objects                   */
+/*                                                                         */
+/* http://www.qedo.org/                                                    */
+/*                                                                         */
+/* Copyright (C) 2002-2004 by the Qedo Team                                */
+/*                                                                         */
+/* This program is free software; you can redistribute it and/or modify it */
+/* under the terms of the GNU General Public License as published by the   */
+/* Free Software Foundation; either version 2 of the License,              */
+/* or (at your option) any later version.                                  */
+/*                                                                         */
+/* This program is distributed in the hope that it will be useful,         */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of          */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                    */
+/* See the GNU General Public License for more details.                    */
+/*                                                                         */
+/* You should have received a copy of the GNU General Public License       */
+/* along with this program; if not, write to the Free Software Foundation, */
+/* Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA             */
+/***************************************************************************/
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#ifndef _QEDO_NO_QOS
+#include "ServantInterceptorDispatcher.h"
+#include "Util.h"
+#include "Output.h"
+#include <fstream>
+
+#ifndef _QEDO_NO_STREAMS
+#include "StreamComponents.h"
+#endif
+
+static char rcsid[] UNUSED = "$Id: ServantInterceptorDispatcher.cpp,v 1.1 2004/07/16 11:56:43 tom Exp $";
+
+namespace Qedo {
+
+ServantInterceptorDispatcher::ServantInterceptorDispatcher()
+{
+
+}
+
+ServantInterceptorDispatcher::~ServantInterceptorDispatcher()
+{
+
+}
+
+void
+ServantInterceptorDispatcher::register_interceptor_for_all(Components::Extension::ServerContainerInterceptor_ptr interceptor)
+{
+	DEBUG_OUT("ServantInterceptorDispatcher: Server COPI registered for all components");
+
+	ServantInterceptorEntry e;
+	e.interceptor = Components::Extension::ServerContainerInterceptor::_duplicate( interceptor );
+
+	Qedo::QedoLock l(all_servant_interceptors_mutex_);
+
+	all_servant_interceptors_.push_back(e);
+
+}
+
+CORBA::Boolean 
+ServantInterceptorDispatcher::call( const char* oper ) 
+{
+	// call registered interceptors
+
+	// return true if the execution shall continue
+	// an out parameter of type any is needed to return a possible return type
+	DEBUG_OUT("ServantInterceptorDispatcher: get call");
+	return true;
+}
+
+Components::Cookie* 
+ServantInterceptorDispatcher::connect( const char* comp_id, const char* name, CORBA::Object_ptr connection, CORBA::Boolean_out con )
+{
+	Components::Cookie* temp_ck;
+	for (unsigned int i = 0; i < all_servant_interceptors_.size(); i++)
+	{
+		try {
+
+			temp_ck = all_servant_interceptors_[i].interceptor->connect( comp_id, name, connection, con);
+
+			if (!con)
+			{
+				return temp_ck;
+				break;
+			};
+		} catch (CORBA::SystemException e)
+		{
+			throw e;
+		} catch ( ... )
+			// catch of user exception is probably missing
+		{
+		}
+	}
+
+
+}
+
+
+CORBA::Object_ptr 
+ServantInterceptorDispatcher::provide_facet( const char* comp_id, const char* name, CORBA::Boolean_out con ) 
+{
+	CORBA::Object_var anObject;
+	for (unsigned int i = 0; i < all_servant_interceptors_.size(); i++)
+	{
+		try {
+
+			anObject = all_servant_interceptors_[i].interceptor->provide_facet( comp_id, name, con);
+
+			if (!con)
+			{
+				return anObject;
+				break;
+			};
+		} catch (CORBA::SystemException e)
+		{
+			throw e;
+		} catch ( ... )
+			// catch of user exception is probably missing
+		{
+		}
+	}
+
+}
+
+
+Components::Cookie* 
+ServantInterceptorDispatcher::bind(const char* comp_id, const char* name,
+                           CORBA::Object_ptr the_sink,
+						   const char* transport_profile,
+						   CORBA::Boolean_out con)
+{
+
+	Components::Cookie* temp_ck;
+	for (unsigned int i = 0; i < all_servant_interceptors_.size(); i++)
+	{
+		try {
+
+			temp_ck = all_servant_interceptors_[i].interceptor->bind( comp_id, name, the_sink, transport_profile, con);
+
+			if (!con)
+			{
+				return temp_ck;
+				break;
+			};
+		} catch (CORBA::SystemException e)
+		{
+			throw e;
+		} catch ( ... )
+			// catch of user exception is probably missing
+		{
+		}
+	}
+
+}
+
+CORBA::Object_ptr 
+ServantInterceptorDispatcher::provide_sink_stream_port( const char* comp_id, const char* name, CORBA::Boolean_out con ) 
+{
+	CORBA::Object_var anObject;
+	for (unsigned int i = 0; i < all_servant_interceptors_.size(); i++)
+	{
+		try {
+
+			anObject = all_servant_interceptors_[i].interceptor->provide_sink_stream_port( comp_id, name, con);
+
+			if (!con)
+			{
+				return anObject;
+				break;
+			};
+		} catch (CORBA::SystemException e)
+		{
+			throw e;
+		} catch ( ... )
+			// catch of user exception is probably missing
+		{
+		}
+	}
+
+}
+
+
+
+
+}; // namespace Qedo
+#endif // _QEDO_NO_QOS
---
+++ trunk/qedo/ComponentContainer/ServantInterceptorDispatcher.h
@@ -0,0 +1,94 @@
+/***************************************************************************/
+/* Qedo - Quality of Service Enabled Distributed Objects                   */
+/*                                                                         */
+/* http://www.qedo.org/                                                    */
+/*                                                                         */
+/* Copyright (C) 2002-2004 by the Qedo Team                                */
+/*                                                                         */
+/* This program is free software; you can redistribute it and/or modify it */
+/* under the terms of the GNU General Public License as published by the   */
+/* Free Software Foundation; either version 2 of the License,              */
+/* or (at your option) any later version.                                  */
+/*                                                                         */
+/* This program is distributed in the hope that it will be useful,         */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of          */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                    */
+/* See the GNU General Public License for more details.                    */
+/*                                                                         */
+/* You should have received a copy of the GNU General Public License       */
+/* along with this program; if not, write to the Free Software Foundation, */
+/* Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA             */
+/***************************************************************************/
+
+
+#ifndef __SERVANT_INTERCEPTOR_DISPATCHER_H__
+#define __SERVANT_INTERCEPTOR_DISPATCHER_H__
+
+#ifndef _QEDO_NO_QOS
+#include <vector>
+#include "Components.h"
+#include "Synchronisation.h"
+#include "Util.h"
+
+
+namespace Qedo {
+
+	class ServantInterceptorEntry {
+	public:
+		Components::Extension::ServerContainerInterceptor_var interceptor;
+		std::string id;
+	};
+
+	typedef std::vector < ServantInterceptorEntry > ServantInterceptorVector;
+
+	class CONTAINERDLL_API ServantInterceptorDispatcher :
+		public virtual Components::Extension::ServantInterceptorRegistration,
+		public virtual CORBA::LocalObject
+	{
+	private:
+
+		/** vector of interceptors registered for all */
+		ServantInterceptorVector all_servant_interceptors_;
+		/** Mutex for all_server_interceptors_ */
+		QedoMutex all_servant_interceptors_mutex_;
+
+		/** vector of interceptors registered for component_id */
+		ServantInterceptorVector for_component_id_servant_interceptors_;
+		/** Mutex for all_servant_interceptors_ */
+		QedoMutex for_component_id_servant_interceptors_mutex_;
+
+		/** reference to the component server */
+	//	Qedo::ComponentServerImpl* component_server_;
+
+	public:
+		ServantInterceptorDispatcher();
+
+		~ServantInterceptorDispatcher();
+
+
+		virtual void
+		register_interceptor_for_all(Components::Extension::ServerContainerInterceptor_ptr interceptor);
+
+	    virtual CORBA::Boolean 
+		call( const char* oper ) ;
+//CCMObject
+		virtual Components::Cookie* connect( const char* comp_id, const char* name, CORBA::Object_ptr connection, CORBA::Boolean_out con ) ;
+    virtual CORBA::Object_ptr provide_facet( const char* comp_id, const char* name, CORBA::Boolean_out con );
+
+
+// StreamCCMObject
+		virtual Components::Cookie* 
+		bind(const char* comp_id, const char* name,
+                           CORBA::Object_ptr the_sink,
+						   const char* transport_profile,
+						   CORBA::Boolean_out con);
+
+		virtual CORBA::Object_ptr 
+		provide_sink_stream_port( const char* comp_id, const char* name, CORBA::Boolean_out con ) ;
+
+
+	};
+};
+
+#endif // _QEDO_NO_QOS
+#endif // __SERVANT_INTERCEPTOR_DISPATCHER_H__
