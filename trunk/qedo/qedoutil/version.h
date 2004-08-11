71174a79ba5e734ff6bf5c0a0179e9429d41f75d
tom <tom@798282e8-cfd4-0310-a90d-ae7fb11434eb>
2004-08-11 11:06:04 +0000

    version 0.7.2

--- trunk/qedo/qedoutil/version.h
+++ trunk/qedo/qedoutil/version.h
@@ -20,4 +20,4 @@
 /* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA */
 /***************************************************************************/
 
-#define QEDO_VERSION "0.7.1"
+#define QEDO_VERSION "0.7.2"
