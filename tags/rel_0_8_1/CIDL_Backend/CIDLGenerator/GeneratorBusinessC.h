c76b0936cdb577dbf90ba79d71b0c2d46e086590
tom <tom@798282e8-cfd4-0310-a90d-ae7fb11434eb>
2005-11-27 12:14:27 +0000

    release 0.8.1

---
+++ tags/rel_0_8_1/CIDL_Backend/CIDLGenerator/GeneratorBusinessC.h
@@ -0,0 +1,82 @@
+/*****************************************************************************/
+/* Qedo - Quality of Service Enabled Distributed Objects                     */
+/*                                                                           */
+/* Copyright (c) 2002/2003 by the Qedo Team                                  */
+/*                                                                           */
+/* http://qedo.berlios.de                                                    */
+/*                                                                           */
+/* This file is part of Qedo Generator                                       */
+/*                                                                           */
+/* Qedo Generator is free software; you can redistribute it and/or modify    */
+/* it under the terms of the GNU General Public License as published by      */
+/* the Free Software Foundation; either version 2 of the License, or         */
+/* (at your option) any later version.                                       */
+/*                                                                           */
+/* Qedo Generator is distributed in the hope that it will be useful,         */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of            */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             */
+/* GNU General Public License for more details.                              */
+/*                                                                           */
+/* You should have received a copy of the GNU General Public License         */
+/* along with Foobar; if not, write to the Free Software                     */
+/* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
+/*                                                                           */
+/*****************************************************************************/
+
+#ifndef __GENERATOR_BUSINESSC_H__
+#define __GENERATOR_BUSINESSC_H__
+
+#include "CPPBase.h"
+#include "Printer.h"
+//#include "GeneratorPersistenceC.h"
+#include <fstream>
+#include <iostream>
+#include <set>
+
+
+namespace QEDO_CIDL_Generator {
+
+
+class GeneratorBusinessC : public virtual CPPBase
+{
+
+private:
+
+	std::string filename_;
+	std::string class_name_;
+	Printer out;
+	bool need_push_;
+	CIDL::CompositionDef_var composition_;
+	IR__::StorageHomeDef_var storagehome_;
+	std::set < std::string > handled_interfaces_;
+//	GeneratorPersistenceC*      pc_generator_;
+
+	void doException(IR__::ExceptionDef_ptr except);
+	void doModule(IR__::ModuleDef_ptr module);
+	void doAttribute(IR__::AttributeDef_ptr attribute);
+	void doOperation(IR__::OperationDef_ptr operation);
+	void doInterface(IR__::InterfaceDef_ptr intface);
+	void doComponent(IR__::ComponentDef_ptr component);
+	void doConsumes(IR__::ConsumesDef_ptr consumes, IR__::ComponentDef_ptr component);
+	void doSink(IR__::SinkDef_ptr sink, IR__::ComponentDef_ptr component);
+	void doHome(IR__::HomeDef_ptr home);
+	void doFactory(IR__::FactoryDef_ptr factory);
+	void doFinder(IR__::FinderDef_ptr finder);
+	void doComposition(CIDL::CompositionDef_ptr composition);
+
+	void gen_executor_locator_factes (IR__::ComponentDef_var a_component, CIDL::CompositionDef_ptr composition);
+
+public:
+
+	GeneratorBusinessC(CIDL::CIDLRepository_ptr repository);
+	~GeneratorBusinessC();
+
+	void generate(std::string target, std::string fileprefix);
+
+};
+
+
+} // namespace
+
+
+#endif
---
+++ tags/rel_0_8_1/CIDL_Backend/CIDLGenerator/cidl_gen.cpp
@@ -0,0 +1,507 @@
+#include "Debug.h"
+#include "GeneratorEIDL.h"
+#include "GeneratorLIDL.h"
+#include "GeneratorBIDL.h"
+#include "GeneratorCCD.h"
+#include "GeneratorCSD.h"
+#include "GeneratorBusinessH.h"
+#include "GeneratorBusinessC.h"
+#include "GeneratorServantH.h"
+#include "GeneratorServantC.h"
+#include "GeneratorPSD.h"
+#include "GeneratorVC7.h"
+#include "GeneratorMakefile.h"
+#include "GeneratorValuetypesH.h"
+#include "GeneratorValuetypesC.h"
+#include "TestMode.h"
+#include "version.h"
+#include "frontend.h"
+
+#include <signal.h>
+#include <iostream>
+#ifdef TAO_ORB
+#include "tao/TypeCodeFactory/TypeCodeFactory_Adapter_impl.h"
+#endif
+using namespace std;
+
+
+//QEDO_ComponentRepository::CIDLRepository_impl *repository;
+CIDL::CIDLRepository_ptr repository;
+
+void
+handle_sigint
+( int signal )
+{
+    std::cout << "Got Crtl-C" << std::endl;
+    std::cout << "Waiting for completion..." << std::endl;
+}
+
+
+void
+printUsage
+(int argc, char **argv)
+{
+    std::cerr << "usage : " << argv[0];
+    std::cerr << " [options] --target <compositionname> filename" << std::endl;
+    std::cerr << "        [--target|-t] <compositionname> : the element to generate code for" << std::endl;
+    std::cerr << "        [--business|-b] : generate business code skeletons" << std::endl;
+    std::cerr << "        [--servant|-s] : generate servant code" << std::endl;
+    std::cerr << "        --out <fileprefix> : idl files will be prefixed with fileprefix" << std::endl;
+    std::cerr << "        --vc7 : generate VC7 projects" << std::endl;
+    std::cerr << "        --mkfile : generate Makefiles" << std::endl;
+    std::cerr << "        -d : generate XML Descriptor skeletons" << std::endl;
+    std::cerr << "        [--help|-h] : print this" << std::endl;
+}
+
+
+int
+main
+( int argc, char **argv )
+{
+	std::cout << "CIDL Generator Version " << GENERATOR_VERSION << std::endl;
+	std::cout << "Qedo Team" << std::endl;
+
+	/*
+	// disable heap checks in debug mode to fasten up the idl compilations
+	*/
+#ifdef _WIN32
+#ifdef _DEBUG
+	// Get current flag
+	int tmpFlag = _CrtSetDbgFlag( _CRTDBG_REPORT_FLAG );
+
+	tmpFlag &= ~_CRTDBG_ALLOC_MEM_DF;
+
+	// Set flag to the new value
+	_CrtSetDbgFlag( tmpFlag );
+#endif
+#endif
+
+	//
+	// get ORB
+	//
+	CORBA::ORB_var orb;
+	try
+	{
+		orb = CORBA::ORB_init ( argc, argv );
+	}
+	catch ( ... )
+	{
+		std::cerr << "Error during ORB_init()" << std::endl;
+		exit ( 1 );
+	}
+
+	//
+	// get POA
+	//
+	PortableServer::POA_var root_poa;
+	try
+	{
+		CORBA::Object_var root_poa_obj = orb->resolve_initial_references ( "RootPOA" );
+		root_poa = PortableServer::POA::_narrow ( root_poa_obj );
+	}
+	catch ( ... )
+	{
+		std::cerr << "Error during getting root POA" << std::endl;
+		orb -> destroy();
+		exit ( 1 );
+	}
+	
+	//
+	// get POA manager and activate it
+	//
+	PortableServer::POAManager_var root_poa_manager;
+	try
+	{
+		root_poa_manager = root_poa -> the_POAManager();
+	}
+	catch ( ... )
+	{
+		std::cerr << "Error getting root POA manager" << std::endl;
+		orb->destroy();
+		exit ( 1 );
+	}
+	root_poa_manager -> activate();
+
+	signal ( SIGINT, handle_sigint );
+
+	bool generateEIDL = true;
+	bool generateLIDL = false;
+	bool generateBusiness = false;
+	bool generateServant = false;
+	bool generatevc7 = false;
+	bool generateMake = false;
+	bool generateDescriptors = false;
+	bool feed_ifr = false;
+	bool read_ifr = false;
+	std::string ifr_ref = "";
+	std::string target;
+	std::string fileprefix = "";
+	std::string target_file_name = argv[argc - 1];
+
+	//
+	// process arguments
+	//
+	if(argc < 2)
+	{
+		printUsage(argc, argv);
+		orb->destroy();
+		exit ( 1 );
+	}
+
+    for(int i = 1; i < argc;)
+    {
+        const char* option = argv[i];
+		if((strcmp(option, "--help") == 0) || (strcmp(option, "-h") == 0))
+		{
+			printUsage(argc, argv);
+			orb->destroy();
+			exit ( 1 );
+		}
+		else if(strcmp(option, "--testmode") == 0)
+		{
+			frontend_replacement_feed ( repository );
+
+			for(int j = i ; j + 1 < argc ; j++)
+				argv[j] = argv[j + 1];
+
+			argc--;
+		}
+		else if((strcmp(option, "--business") == 0) || (strcmp(option, "-b") == 0))
+		{
+			/* On Windows the executor needs to link equivalent and local */
+			/* On Linux the executor doesn't need it to be linked */
+			/* default the equivalent IDL is on but turned off here on Linux */
+
+			generateBusiness = true;
+#ifdef _WIN32
+			generateLIDL = true;
+#else
+			generateEIDL = false;
+#endif
+			for(int j = i ; j + 1 < argc ; j++)
+				argv[j] = argv[j + 1];
+
+			argc--;
+		}
+		else if((strcmp(option, "--servant") == 0) || (strcmp(option, "-s") == 0))
+		{
+			generateServant = true;
+			generateLIDL = true;
+			generateEIDL = true;
+			for(int j = i ; j + 1 < argc ; j++)
+			argv[j] = argv[j + 1];
+
+			argc--;
+		}
+		else if(strcmp(option, "--vc7") == 0)
+		{
+			generatevc7 = true;
+            
+            for(int j = i ; j + 1 < argc ; j++)
+                argv[j] = argv[j + 1];
+            
+            argc--;
+		}
+		else if(strcmp(option, "--mkfile") == 0)
+		{
+			generateMake = true;
+            
+            for(int j = i ; j + 1 < argc ; j++)
+                argv[j] = argv[j + 1];
+            
+            argc--;
+		}
+		else if(strcmp(option, "-d") == 0)
+		{
+			generateDescriptors = true;
+            
+            for(int j = i ; j + 1 < argc ; j++)
+                argv[j] = argv[j + 1];
+            
+            argc--;
+		}
+		else if((strcmp(option, "--target") == 0) || (strcmp(option, "-t") == 0))
+		{
+			if(i + 1 >= argc)
+			{
+				std::cerr << "argument expected for --target" << std::endl;
+				orb -> destroy();
+				exit ( 1 );
+			}
+
+            target = argv[i + 1];
+            
+            for(int j = i ; j + 2 < argc ; j++)
+                argv[j] = argv[j + 2];
+            
+            argc -= 2;
+		}
+		else if(strcmp(option, "--out") == 0)
+		{
+			if(i + 1 >= argc)
+			{
+				std::cerr << "argument expected for --out" << std::endl;
+				orb -> destroy();
+				exit ( 1 );
+			}
+
+            fileprefix = argv[i + 1];
+            
+            for(int j = i ; j + 2 < argc ; j++)
+                argv[j] = argv[j + 2];
+            
+            argc -= 2;
+		}
+		else if(strcmp(option, "--feed-ifr") == 0)
+		{
+			if(i + 1 >= argc)
+			{
+				std::cerr << "argument expected for --feed-ifr" << std::endl;
+				orb -> destroy();
+				exit ( 1 );
+			}
+
+            feed_ifr = true;
+            ifr_ref = argv[i + 1];
+            
+            for(int j = i ; j + 2 < argc ; j++)
+                argv[j] = argv[j + 2];
+            
+            argc -= 2;
+		}
+		else if(strcmp(option, "--read-ifr") == 0)
+		{
+			if(i + 1 >= argc)
+			{
+				std::cerr << "argument expected for --read-ifr" << std::endl;
+				orb -> destroy();
+				exit ( 1 );
+			}
+
+            read_ifr = true;
+            ifr_ref = argv[i + 1];
+            
+            for(int j = i ; j + 2 < argc ; j++)
+                argv[j] = argv[j + 2];
+            
+            argc -= 2;
+		}
+		else
+		{
+			i++;
+		}
+	}
+
+
+	if(target.empty() && !feed_ifr)
+	{
+		printUsage(argc, argv);
+		orb->destroy();
+		exit ( 1 );
+	}
+
+	if (feed_ifr) {
+		try {
+			// read ior provide object reference
+			std::ifstream ior_file;
+			ior_file.open(ifr_ref.c_str());
+
+			std::cout << "Feeder: Reading Repository Reference: " << ifr_ref << "\n";
+			char obj_string[2048];
+			ior_file >> obj_string;
+
+			CORBA::Object_var anobject = orb -> string_to_object (obj_string);
+			repository = CIDL::CIDLRepository::_narrow(anobject);
+
+		} catch (...)
+		{
+			cout << "Cannot create reference to IFR Repository!\n";
+			orb->destroy();
+			exit ( 1 );
+		};
+	} else
+	{
+		// create repository internally
+		repository = CIDL::CIDLRepository::_narrow( (new QEDO_ComponentRepository::CIDLRepository_impl ( orb, root_poa )) -> _this());
+	}
+
+	try {
+		// feed repository
+		frontend_feed ( argc, argv, repository );
+	} catch (...) 
+	{
+		std::cout << "Problem in parser occured ... exiting." << std::endl;
+		orb->destroy();
+		exit ( 1 );
+	}
+
+	if (read_ifr) {
+		try {
+			// read ior provide object reference
+			std::ifstream ior_file;
+			ior_file.open(ifr_ref.c_str());
+
+			std::cout << "Back-ends: Reading Repository Reference: " << ifr_ref << "\n";
+			char obj_string[2048];
+			ior_file >> obj_string;
+
+			CORBA::Object_var anobject = orb -> string_to_object (obj_string);
+			repository = CIDL::CIDLRepository::_narrow(anobject);
+
+			std::cout << "Using model from IFR and Ignoring idl/cidl input file.\n";
+
+		} catch (...)
+		{
+			cout << "Cannot create reference to IFR Repository!\n";
+			orb->destroy();
+			exit ( 1 );
+		};
+	}
+
+
+	if(generateEIDL)
+	{
+		// generate equivalent IDL
+		std::cout << "Generating equivalent IDL for " << target << std::endl;
+		QEDO_CIDL_Generator::GeneratorEIDL *eidl_generator =
+			new QEDO_CIDL_Generator::GeneratorEIDL(repository, orb.in());
+		eidl_generator->generate(target, fileprefix);
+		eidl_generator->destroy();
+	}
+
+	if(generateLIDL)
+	{
+		// generate local IDL
+		std::cout << "Generating local IDL for " << target << std::endl;
+		QEDO_CIDL_Generator::GeneratorLIDL *lidl_generator =
+			new QEDO_CIDL_Generator::GeneratorLIDL(repository);
+		lidl_generator->generate(target, fileprefix);
+		lidl_generator->destroy();
+	}
+
+	if(generateBusiness)
+	{
+		// generate local business IDL
+		std::cout << "Generating local business IDL for " << target << std::endl;
+		QEDO_CIDL_Generator::GeneratorBIDL *bidl_generator =
+			new QEDO_CIDL_Generator::GeneratorBIDL(repository);
+		bidl_generator->generate(target, fileprefix);
+		bidl_generator->destroy();
+
+		// generate business header
+		std::cout << "Generating business code header for " << target << std::endl;
+		QEDO_CIDL_Generator::GeneratorBusinessH *bh_generator =
+			new QEDO_CIDL_Generator::GeneratorBusinessH(repository);
+		bh_generator->generate(target, fileprefix);
+		bh_generator->destroy();
+	
+		// generate business code
+		std::cout << "Generating business code for " << target << std::endl;
+		QEDO_CIDL_Generator::GeneratorBusinessC *bc_generator =
+			new QEDO_CIDL_Generator::GeneratorBusinessC(repository);
+		bc_generator->generate(target, fileprefix);
+		bc_generator->destroy();
+
+		// generate valuetype header
+		std::cout << "Generating valuetype header" << std::endl;
+		QEDO_CIDL_Generator::GeneratorValuetypesH *vth_generator =
+			new QEDO_CIDL_Generator::GeneratorValuetypesH(repository);
+		vth_generator->generate(target, fileprefix);
+		vth_generator->destroy();
+
+		// generate valuetype code
+		std::cout << "Generating valuetype code" << std::endl;
+		QEDO_CIDL_Generator::GeneratorValuetypesC *vtc_generator =
+			new QEDO_CIDL_Generator::GeneratorValuetypesC(repository);
+		vtc_generator->generate(target, fileprefix);
+		vtc_generator->destroy();
+		/*
+		// generate persistent header
+		std::cout << "Generating persistent code header for " << target << std::endl;
+		QEDO_CIDL_Generator::GeneratorPersistenceH *ph_generator =
+			new QEDO_CIDL_Generator::GeneratorPersistenceH(repository);
+		ph_generator->generate(target, fileprefix);
+		ph_generator->destroy();
+
+		// generate persistent code
+		std::cout << "Generating persistent code for " << target << std::endl;
+		QEDO_CIDL_Generator::GeneratorPersistenceC *pc_generator =
+			new QEDO_CIDL_Generator::GeneratorPersistenceC(repository);
+		pc_generator->generate(target, fileprefix);
+		pc_generator->destroy();
+		
+		// generate persistent descriptor
+		std::cout << "Generating persistent descriptor " << target << std::endl;
+		QEDO_CIDL_Generator::GeneratorPSD *psd_generator =
+			new QEDO_CIDL_Generator::GeneratorPSD(repository);
+		psd_generator->generate(target, fileprefix);
+		psd_generator->destroy();
+		*/
+	}
+
+	if(generateDescriptors)
+	{
+		// generate CORBA Component Descriptor
+		std::cout << "Generating CORBA Component Descriptor for " << target << std::endl;
+		QEDO_CIDL_Generator::GeneratorCCD *ccd_generator =
+			new QEDO_CIDL_Generator::GeneratorCCD(repository);
+		ccd_generator->generate(target, fileprefix);
+		ccd_generator->destroy();
+
+		// generate Software Package Descriptor
+		std::cout << "Generating Software Package Descriptor for " << target << std::endl;
+		QEDO_CIDL_Generator::GeneratorCSD *csd_generator =
+			new QEDO_CIDL_Generator::GeneratorCSD(repository);
+		csd_generator->generate(target, fileprefix);
+		csd_generator->destroy();
+	}
+
+	if(generateServant)
+	{
+		// generate servant header
+		std::cout << "Generating servant code header for " << target << std::endl;
+		QEDO_CIDL_Generator::GeneratorServantH *sh_generator =
+			new QEDO_CIDL_Generator::GeneratorServantH(repository);
+		sh_generator->generate(target, fileprefix);
+		sh_generator->destroy();
+
+		// generate servant code
+		std::cout << "Generating servant code for " << target << std::endl;
+		QEDO_CIDL_Generator::GeneratorServantC *sc_generator =
+			new QEDO_CIDL_Generator::GeneratorServantC(repository);
+		sc_generator->generate(target, fileprefix);
+		sc_generator->destroy();
+	}
+
+	if(generatevc7)
+	{
+		// generate VC7 projects
+		std::cout << "Generating VC7 projects for " << target << std::endl;
+		QEDO_CIDL_Generator::GeneratorVC7 *vc7_generator =
+			new QEDO_CIDL_Generator::GeneratorVC7(repository);
+		vc7_generator->target_file_name_ = target_file_name;
+		vc7_generator->generate(target, fileprefix);
+		vc7_generator->destroy();
+	}
+
+	if(generateMake)
+	{
+		// generate Makefiles
+		std::cout << "Generating Makefiles for " << target << std::endl;
+		QEDO_CIDL_Generator::GeneratorMakefile *makefile_generator =
+			new QEDO_CIDL_Generator::GeneratorMakefile(repository);
+		makefile_generator->target_file_name_ = target_file_name;
+		makefile_generator->generate(target, fileprefix);
+		makefile_generator->destroy();
+	}
+
+
+	signal ( SIGINT, SIG_DFL );
+
+	//repository -> destroy_repository();
+
+	orb -> shutdown ( false );
+
+
+	return 0;
+}
