ec0f0e2d6e3f9e8265ad1782df58d06813a4d697
tom <tom@798282e8-cfd4-0310-a90d-ae7fb11434eb>
2006-01-05 17:12:08 +0000

    removed obsolete version files added timestamp to output of cidl_gen

--- trunk/qedo/CIDL_Backend/CIDLGenerator/cidl_gen.cpp
+++ trunk/qedo/CIDL_Backend/CIDLGenerator/cidl_gen.cpp
@@ -58,7 +58,7 @@ int
 main
 ( int argc, char **argv )
 {
-	std::cout << "CIDL Generator Version " << QEDO_VERSION << std::endl;
+	std::cout << "CIDL Generator Version " << QEDO_VERSION << " (built: " << __TIMESTAMP__ << ")" << std::endl;
 	std::cout << "Qedo Team" << std::endl;
 
 	/*
